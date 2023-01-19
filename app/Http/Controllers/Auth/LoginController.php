2df15be980913610e37aed771092796042810974
zuramai <saugi.dev@gmail.com>
2023-01-19 13:11:32 +0700

    fix: login username

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -37,4 +37,8 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function username() {
+        return 'username';
+    }
 }
